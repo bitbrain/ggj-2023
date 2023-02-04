5a07a2e10683ff1be993b8532db660a02721ad25
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2023-02-04 21:19:21 +0000

    add water splash

--- godot/gameplay/water/WaterDrop.gd
+++ godot/gameplay/water/WaterDrop.gd
@@ -3,10 +3,15 @@ extends RigidBody2D
 @export var expires = true
 
 @onready var area = $Area2D
+@onready var ground_control:GroundControl = $GroundControl
+@onready var sprite := $Sprite2D
 
 func _ready() -> void:
 	area.area_entered.connect(func(area): queue_free())
 	if expires:
 		$ExpiryTimer.timeout.connect(queue_free)
 		$ExpiryTimer.start()
+	
+	# trigger splash animation on touch ground
+	ground_control.on_touch_ground.connect(func(): sprite.play("default"))
 
