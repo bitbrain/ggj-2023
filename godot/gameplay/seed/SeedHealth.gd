da1ab9eee6aa902c3919391861eeb4bfa05d2e7e
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2023-02-04 15:07:36 +0000

    Introduce lava pool

--- godot/gameplay/seed/SeedHealth.gd
+++ godot/gameplay/seed/SeedHealth.gd
@@ -3,6 +3,8 @@ extends Area2D
 signal seed_dead
 
 @export var max_health := 3
+
+@onready var insta_death_area = $InstaDeathArea
 	
 var current_health := max_health:
 	set(v):
@@ -12,11 +14,15 @@ var current_health := max_health:
 
 func _ready() -> void:
 	area_entered.connect(_on_hazard_entered)
+	insta_death_area.area_entered.connect(_insta_death)
 	
 func _on_hazard_entered(hazard:Area2D) -> void:
 	if current_health > 0:
 		current_health = current_health - 1
 		hazard.get_parent().queue_free()
+		
+func _insta_death(hazard:Area2D) -> void:
+	current_health = 0
 		
 func reset() -> void:
 	current_health = max_health
--- godot/gameplay/water/WaterDrop.gd
+++ godot/gameplay/water/WaterDrop.gd
@@ -2,7 +2,10 @@ extends RigidBody2D
 
 @export var expires = true
 
+@onready var area = $Area2D
+
 func _ready() -> void:
+	area.area_entered.connect(queue_free)
 	if expires:
 		$ExpiryTimer.timeout.connect(queue_free)
 		$ExpiryTimer.start()
